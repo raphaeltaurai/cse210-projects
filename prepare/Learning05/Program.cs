c8ad8eba4bbb2f28ca53fe98900c335f68603198
raphaeltaurai <shawntaurai@byui.edu>
2024-12-11 02:35:09 +0200

    changes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("Calisto Gomez", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment a2 = new MathAssignment("Chiremba Rimuka", "Long Division", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mahuni Tafadzwa", "Home Ecnomics", "Food Storage");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
